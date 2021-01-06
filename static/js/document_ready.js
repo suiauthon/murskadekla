6f902a5331dfdcb594ad26f19493492963d1c666
suiauthon <carek.marko@gmail.com>
2021-01-06 14:43:45 +0100

    implemented footer and header

--- static/js/document_ready.js
+++ static/js/document_ready.js
@@ -1,4 +1,19 @@
 $(document).ready(function () {
+    navbar_setup();
+
     //stavlja sve href na linkove
     links_setup();
 });
+
+function navbar_setup() {
+    const menu = document.querySelector('#mobile-menu');
+    const menuLinks = document.querySelectorAll('.navbar');
+
+    menu.addEventListener('click', function () {
+        menu.classList.toggle('is-active');
+        menuLinks.forEach(function (item) {
+            item.classList.toggle('active');
+        })
+    });
+}
+
